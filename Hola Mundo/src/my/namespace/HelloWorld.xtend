943a81445c5a7788b88f5425b45abf525c8afafd
Agustin Brandoni <agusbrand@gmail.com>
2014-04-08 23:22:25 -0300

    hola mundo de Agustin

---
+++ Hola Mundo/src/my/namespace/HelloWorld.xtend
@@ -0,0 +1,9 @@
+package my.namespace
+
+class HelloWorld {
+	def static void main(String[] args) {
+		println('hello World')
+	}
+	
+	
+}
---
+++ Hola Mundo/xtend-gen/my/namespace/HelloWorld.java
@@ -0,0 +1,10 @@
+package my.namespace;
+
+import org.eclipse.xtext.xbase.lib.InputOutput;
+
+@SuppressWarnings("all")
+public class HelloWorld {
+  public static void main(final String[] args) {
+    InputOutput.<String>println("hello World");
+  }
+}
