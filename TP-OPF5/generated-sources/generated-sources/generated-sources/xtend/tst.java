feec8183a3e42671f015904ec967f44052322596
Naiara Acosta <acosta.naiara@gmail.com>
2014-04-28 00:57:18 -0300

    Nueva version de Test Case mas acomodado

---
+++ TP-OPF5/generated-sources/generated-sources/generated-sources/xtend/tst.java
@@ -0,0 +1,73 @@
+import org.junit.Before;
+import org.junit.Test;
+
+@SuppressWarnings("all")
+public class tst {
+  private /* Jugador */Object jugador;
+  
+  private /* Partido */Object partido;
+  
+  private /* Estandar */Object tipoIncEstandar;
+  
+  private /* Condicional */Object tipoIncCondicional;
+  
+  private /* Solidaria */Object tipoIncSolidaria;
+  
+  @Before
+  public void init() {
+    throw new Error("Unresolved compilation problems:"
+      + "\nJugador cannot be resolved."
+      + "\nPartido cannot be resolved."
+      + "\nEstandar cannot be resolved."
+      + "\nCondicional cannot be resolved."
+      + "\nSolidaria cannot be resolved.");
+  }
+  
+  @Test
+  public void inscribirJugadorModoEstandarListaMenor10() {
+    throw new Error("Unresolved compilation problems:"
+      + "\ninscribite cannot be resolved");
+  }
+  
+  @Test
+  public void inscribirJugadorModoEstandarListaMayor10() {
+    throw new Error("Unresolved compilation problems:"
+      + "\nInscribite cannot be resolved");
+  }
+  
+  @Test
+  public void inscribirJugadorModoSolidarioListaMenor10() {
+    throw new Error("Unresolved compilation problems:"
+      + "\nInscribite cannot be resolved");
+  }
+  
+  @Test
+  public void inscribirJugadorModoSolidarioListaMayor10() {
+    throw new Error("Unresolved compilation problems:"
+      + "\nInscribite cannot be resolved");
+  }
+  
+  @Test
+  public void inscribirJugadorModoCondicionalCumpleCondicion() {
+    throw new Error("Unresolved compilation problems:"
+      + "\nInscribite cannot be resolved");
+  }
+  
+  @Test
+  public void inscribirJugadorModoCondicionalNoCumpleCondicion() {
+    throw new Error("Unresolved compilation problems:"
+      + "\nInscribite cannot be resolved");
+  }
+  
+  @Test
+  public void inscribirJugadorModoEstandarListaMayor10ConUnSolidario() {
+    throw new Error("Unresolved compilation problems:"
+      + "\nInscribite cannot be resolved");
+  }
+  
+  @Test
+  public void inscribirJugadorModoEstandarListaMayor10ConMasSolidario() {
+    throw new Error("Unresolved compilation problems:"
+      + "\nInscribite cannot be resolved");
+  }
+}
