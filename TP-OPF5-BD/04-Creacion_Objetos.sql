5420eeba22ca0b9bbd58cfe8761168ca8c6b690d
agusbrand <agusbrand@gmail.com>
2014-10-21 01:03:38 -0300

    Fix

--- TP-OPF5-BD/04-Creacion_Objetos.sql
+++ TP-OPF5-BD/04-Creacion_Objetos.sql
@@ -2,4 +2,4 @@
 CREATE FUNCTION DDS_F5.jugadores_malos ()
 RETURNS TABLE (id_jugador INT,nombre VARCHAR(50),mail VARCHAR(100),nivelDeJuego INT,apodo VARCHAR(50),fecha_Nacimiento DATE)
 READS SQL DATA
-RETURN TABLE ( SELECT * FROM DDS_F5.jugador );
+RETURN TABLE ( SELECT * FROM DDS_F5.jugador WHERE niveldejuego < 5);
